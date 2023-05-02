96a675c0243bf6bfeb7806af453962a96b787f63
jang-hyuk <wkdgur1380@gmail.com>
2023-05-02 13:32:43 +0900

    fix: dependency 수정

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,9 +1,16 @@
 /// <reference types="vitest" />
+import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      vue: resolve('../../node_modules/vue'),
+    },
+    dedupe: ['vue'],
+  },
   test: {
     reporters: ['verbose'],
     include: ['tests/**/*.test.ts'],
